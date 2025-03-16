7314fda0b21dc09f39ec4cd7d84d9d6dc92493b9
hector avellaneda <ahectore@gmail.com>
2025-03-16 17:13:41 -0300

    init project

---
+++ src/test/java/com/soft/technology/transactions_management/TransactionsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.technology.transactions_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
